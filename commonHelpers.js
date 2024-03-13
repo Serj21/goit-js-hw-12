a54251587e92b24a35d28284b8bc7e04905d2bfc
Serj21 <66782761+Serj21@users.noreply.github.com>
2024-03-13 09:51:30 +0000

    Deploying to gh-pages from @ Serj21/goit-js-hw-12@6dbd4017f5989793de4e539ae4693f5393275868 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,26 +1,26 @@
-import{s as d,i}from"./assets/vendor-8ce50246.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&o(n)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const h="42822558-20d699dce07778b8a952c17c9",c=document.querySelector(".loader");function u(a){const r="https://pixabay.com/api/",s=new URLSearchParams({key:h,q:a,image_type:"photo",orientation:"horizontal",safesearch:!0}),o=`${r}?${s}`;return fetch(o).then(e=>{if(!e.ok)throw new Error("Failed to fetch images");return e.json()}).then(e=>e.hits).catch(e=>{throw new Error("Failed to fetch images")})}function m(){c.classList.remove("hidden")}function f(){c.classList.add("hidden")}const l=document.querySelector(".gallery"),p=new d(".gallery a",{captionsData:"alt",captionDelay:250});function g(){l.innerHTML=""}function y(a){a.forEach(r=>{const s=`
+import{A as E,s as b,i as a}from"./assets/vendor-f41429aa.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))n(t);new MutationObserver(t=>{for(const r of t)if(r.type==="childList")for(const d of r.addedNodes)d.tagName==="LINK"&&d.rel==="modulepreload"&&n(d)}).observe(document,{childList:!0,subtree:!0});function o(t){const r={};return t.integrity&&(r.integrity=t.integrity),t.referrerPolicy&&(r.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?r.credentials="include":t.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(t){if(t.ep)return;t.ep=!0;const r=o(t);fetch(t.href,r)}})();const w=E.create({baseURL:"https://pixabay.com/api/",params:{key:"42822558-20d699dce07778b8a952c17c9"}}),m=document.querySelector(".loader");async function f(s,e){const o={q:s,per_page:15,page:e,image_type:"photo",orientation:"horizontal",safesearch:!0};try{return(await w.get("",{params:o})).data}catch{throw new Error("Failed to fetch images")}}function g(){m.classList.remove("hidden")}function p(){m.classList.add("hidden")}const l=document.querySelector(".gallery"),q=new b(".gallery a",{captionsData:"alt",captionDelay:250});function M(){l.innerHTML=""}function y(s){s.forEach(e=>{const o=`
     <div class="image-card">
-        <a href="${r.largeImageURL}" data-lightbox="gallery-item" data-title="${r.tags}">
-            <img src="${r.webformatURL}" alt="${r.tags}">
+        <a href="${e.largeImageURL}" data-lightbox="gallery-item" data-title="${e.tags}">
+            <img src="${e.webformatURL}" alt="${e.tags}">
         </a>
         <div class="image-info">
         <div class ="image-desc">
         <h2 class = "image-header">Likes:</h2>
-        <p class="header-value"> ${r.likes}</p>
+        <p class="header-value"> ${e.likes}</p>
         </div>
          <div class ="image-desc">
         <h2 class = "image-header">Views:</h2>
-        <p class="header-value"> ${r.views}</p>
+        <p class="header-value"> ${e.views}</p>
         </div>
          <div class ="image-desc">
         <h2 class = "image-header">Comments:</h2>
-        <p class="header-value"> ${r.comments}</p>
+        <p class="header-value"> ${e.comments}</p>
         </div>
          <div class ="image-desc">
-        <h2 class = "image-header">${r.downloads}</h2>
-        <p class="header-value"> ${r.downloads}</p>
+        <h2 class = "image-header">${e.downloads}</h2>
+        <p class="header-value"> ${e.downloads}</p>
         </div>
         </div>
     </div>
-`;l.insertAdjacentHTML("beforeend",s)}),p.refresh()}const v=document.querySelector("#search-form"),L=document.querySelector("#search-input");i.settings({timeout:2e3,position:"topRight",transitionIn:"fadeInUp",transitionOut:"fadeOutDown"});v.addEventListener("submit",a=>{a.preventDefault();const r=L.value.trim();if(r===""){i.error({title:"Error",message:" Please enter image name"});return}m(),g(),u(r).then(s=>{s.length===0?i.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"}):y(s),f()}).catch(s=>{i.error({title:"Error",message:"Failed to fetch images"})})});
+`;l.insertAdjacentHTML("beforeend",o)}),q.refresh()}console.log(5);const $=document.querySelector("#search-form"),O=document.querySelector("#search-input"),u=document.querySelector(".js-btn-load");let i,c,v;u.addEventListener("click",S);a.settings({timeout:2e3,position:"topRight",transitionIn:"fadeInUp",transitionOut:"fadeOutDown"});$.addEventListener("submit",async s=>{if(s.preventDefault(),i=1,c=O.value.trim(),l.innerHTML="",h(),c===""){a.error({title:"Error",message:" Please enter image name"});return}g(),M();try{const e=await f(c,i);e.hits.length===0?a.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"}):(y(e.hits),v=Math.ceil(e.totalHits/15)),p(),L()}catch{a.error({title:"Error",message:"Failed to fetch images"})}});async function S(s){i+=1,h(),g();try{const e=await f(c,i);y(e.hits),x()}catch{a.error({title:"Error",message:"Failed to fetch images"})}p(),L()}function L(){i>=v?(h(),a.error({title:"Error",message:"We're sorry, but you've reached the end of search results."})):P()}function h(){u.classList.add("hidden")}function P(){u.classList.remove("hidden")}function x(){const e=l.firstElementChild.getBoundingClientRect().height*2;scrollBy({behavior:"smooth",top:e})}
 //# sourceMappingURL=commonHelpers.js.map
